48ab1f8b368a26f26b02c6fb764d623732c2a42e
LeeInGyu <developLee20@gmail.com>
2025-05-13 22:34:18 +0900

    feat: 대사의 다양성을 위한 Enum 추가

---
+++ core/enums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+import random
+
+class GoodJobMessage(Enum):
+    GREAT = "대단하다 삐!"
+    AWESOME = "엄청나다 삐!"
+    CRAZY = "미쳤다 삐!"
+    REST = "이제 좀 쉬어라 삐!"
+    LOVE = "반했다 삐!"
+    WOW = "감탄했다 삐!"
+
+    @classmethod
+    def random(cls):
+        return random.choice(list(cls)).value
