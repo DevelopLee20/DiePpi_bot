f6648ee8ed9757d65b8ab61be0601ed6a7b49576
LeeInGyu <developLee20@gmail.com>
2025-05-30 23:49:43 +0900

    style: pre-commit으로 포멧 수정

--- cogs/role_change.py
+++ cogs/role_change.py
@@ -1,15 +1,16 @@
 import discord
-
 from discord.ext import commands
-from db.study_collection import StudyCollection
-from core.enums import Role, Mode
+
+from core.enums import Mode, Role
 from core.env import env
 from core.messages import upgrade_role_message
+from db.study_collection import StudyCollection
+
 
 class RoleChange(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+
         if env.MODE == Mode.PROD.value:
             print("☑️ PROD mode.")
             self.study_channel_name = "공부방"
@@ -18,22 +19,29 @@ class RoleChange(commands.Cog):
             print("☑️ DEV mode.")
             self.study_channel_name = "디스코드-봇-만드는-채널"
             self.alert_channel_name = "디스코드-봇-만드는-채널"
-        
+
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         guild = member.guild
-        
+
         alert_channel = discord.utils.get(
             guild.text_channels, name=self.alert_channel_name
         )
-        
-        total_study_min = await StudyCollection.find_total_study_min_in_today(str(member.id))
-        
+
+        total_study_min = await StudyCollection.find_total_study_min_in_today(
+            str(member.id)
+        )
+
         if total_study_min > 180:
-            role = discord.utils.get(guild.roles, name=Role.DEVELOPMENT_FAIRY.value)  # 개발 요정 역할
+            role = discord.utils.get(
+                guild.roles, name=Role.DEVELOPMENT_FAIRY.value
+            )  # 개발 요정 역할
             if role and role not in member.roles:
                 await member.add_roles(role)
-                await alert_channel.send(upgrade_role_message(member.mention, role.name))
+                await alert_channel.send(
+                    upgrade_role_message(member.mention, role.name)
+                )
+
 
 async def setup(bot):
     await bot.add_cog(RoleChange(bot))
--- core/enums.py
+++ core/enums.py
@@ -5,6 +5,7 @@ class Mode(Enum):
     PROD = "PROD"
     DEV = "DEV"
 
+
 class Role(Enum):
     STUDY_MEMBER = "study Member"
     DEVELOPMENT_FAIRY = "개발 요정"
