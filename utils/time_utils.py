c46ae85a448cf0f5baeb5d87ae38282b0f559b8d
LeeInGyu <developlee20@gmail.com>
2025-05-30 19:21:32 +0900

    refactor: 누적 공부량 출력 수정 (#15)
    Closes: #15

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,4 +1,4 @@
 def min_to_hhmm_str(minutes: int) -> str:
     hours = minutes // 60
     mins = minutes % 60
-    return f"{hours}시간 {mins}분"
+    return f"{hours}시간 {mins}분" if hours > 0 else f"{mins}분"
