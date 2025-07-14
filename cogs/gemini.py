e79d288e42e8dd66666ebfb2fcac620b9f3afab4
LeeInGyu <Developlee20@gmail.com>
2025-07-14 16:44:51 +0900

    Refactor: GeminiClient 상태코드를 함께 반환하도록 변경

--- cogs/gemini.py
+++ cogs/gemini.py
@@ -7,40 +7,42 @@ from core.messages import gemini_response_message
 
 
 class GeminiClient:
-    def __init__(self):
+    def __init__(self, instruction: str):
         self.client = None
+        self.instruction = instruction
 
     async def initialize(self):
         client = genai.Client(api_key=env.GEMINI_API_KEY)
 
         gemini_chat = client.chats.create(
             model="gemini-2.0-flash-lite",
-            config=types.GenerateContentConfig(
-                system_instruction="50자 이내;간단한 설명;문장끝은 항상 삐!"
-            ),
+            config=types.GenerateContentConfig(system_instruction=self.instruction),
         )
 
         self.client = gemini_chat
 
-    async def create_gemini_message(self, input_word: str, ctx) -> str:
+    async def create_gemini_message(self, input_word: str, ctx) -> tuple[bool, str]:
         if self.client is None:
             await self.initialize()
         try:
-            return self.client.send_message(input_word).text
+            return True, self.client.send_message(input_word).text
         except Exception as e:
-            await ctx.send(f"GEMINI 오류 발생: {e}")
-            return ""
+            return False, f"GEMINI 오류 발생: {e}"
 
 
 class GeminiCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.gemini = GeminiClient()
+        self.gemini = GeminiClient("50자 이내;간단한 설명;문장끝은 항상 삐!")
 
     @commands.command(name="단어검색")
     async def gemini_response_command(self, ctx, input_word: str):
-        response = await self.gemini.create_gemini_message(input_word, ctx)
-        text = gemini_response_message(ctx.author.mention, response)
+        status_code, response = await self.gemini.create_gemini_message(input_word, ctx)
+        if status_code:
+            text = gemini_response_message(ctx.author.mention, response)
+        else:
+            text = response
+
         await ctx.send(text)
 
 
