99a7b498cd5e04d49a53fb08b2ae909cc7588ed3
Developlee20 <developlee20@gmail.com>
2025-10-08 18:13:15 +0900

    환경변수 수정

--- core/env.py
+++ core/env.py
@@ -1,11 +1,18 @@
 from pydantic_settings import BaseSettings
 
+from core.enums import Mode
+
 
 class Env(BaseSettings):
     TOKEN: str
     MONGO_DB: str
-    MODE: str
+    MODE: Mode
     GEMINI_API_KEY: str
+    DB_NAME: str = "base"
+    GEMINI_MODEL: str
+    GEMINI_WORD_SEARCH_INSTRUCTION: str
+    GEMINI_STUDY_ENCOURAGEMENT_INSTRUCTION: str
+    COMMAND_PREFIX: str = "/"
 
     class Config:
         env_file = ".env"
